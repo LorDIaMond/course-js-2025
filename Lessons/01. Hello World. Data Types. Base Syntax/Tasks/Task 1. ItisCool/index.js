588d1f611f11603fb59477a8199b233384e191dc
Ainur <ainur9173944784@gmail.com>
2025-09-06 00:03:55 +0300

    Tasks 1-2 completed (test commit)

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 1. ItisCool/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 1. ItisCool/index.js
@@ -1,3 +1,30 @@
-const itisCool = (begin, end) => {};
+const itisCool = (begin, end) => {
+    if (begin > end) {
+        return;
+    }
+
+    for(begin; begin <= end; begin += 1) {
+        if (begin % 3 === 0 &&  begin % 5 === 0)
+            console.log('ItisCool');
+        else if (begin % 3 === 0)
+            console.log('Itis');
+        else if (begin % 5 === 0)
+            console.log('Cool');
+        else
+            console.log(begin);
+    }
+};
 
 export default itisCool;
+
+// const start = 11;
+// const notStart = 20;
+
+// for(start; notStart; start += 1) {
+//         if (start % 3 === 0 &&  start % 5 === 0)
+//             console.log('ItisCool');
+//         else if (start % 3 === 0)
+//             console.log('Itisdd');
+//         else if (start % 5 === 0)
+//             console.log('Cool');
+// }
