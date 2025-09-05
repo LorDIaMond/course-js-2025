9300b4547a5e3e3788227ec8574f6eadd8bc9e67
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 00:55:04 +0300

    first HW redone

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -2,14 +2,14 @@ const isThreePower = (num) => {
     if (num <= 0) {
         return false;
     }
-    let a = 1
-    while(a < num){
-        a *= 3
-        if (a === num){
-            return true
+    let a = 1;
+    while (a <= num) {
+        if (a === num) {
+            return true;
         }
-        return false
+        a *= 3;
     }
+    return false;
 };
 
 export default isThreePower;
