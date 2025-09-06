e050677fab124137f69eaed7e7caf6294c66bf83
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 23:08:44 +0300

    01.c.4 done

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 4. RoundAndRootNumber/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 4. RoundAndRootNumber/index.js
@@ -1,6 +1,12 @@
 // степень округления числа
 const ROUNDING_DEGREE = 100;
 
-const roundAndRootNumber = (num) => {};
+const roundAndRootNumber = (num) => {
+    if (num < 0){
+        return 'error'
+    }
+    let result = String(Math.floor(String(Math.sqrt(num)) * 100) / 100);
+    return result;
+};
 
 export default roundAndRootNumber;
