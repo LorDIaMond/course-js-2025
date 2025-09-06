9b2cbc99eb14e990307411f41a3e95a7a49946a6
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 20:58:09 +0300

    01.h.3 done

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,7 +1,11 @@
 const sumSquareDifference = (num) => {
-    // Начало
-    
-    // Конец
+    let sumOfSquares = 0;
+    let squaredSum = 0;
+    for (let i = 1; i <= num; i++) {
+        sumOfSquares += i ** 2;
+        squaredSum += i;
+    }
+    return squaredSum ** 2 - sumOfSquares;
 };
 
 export default sumSquareDifference;
