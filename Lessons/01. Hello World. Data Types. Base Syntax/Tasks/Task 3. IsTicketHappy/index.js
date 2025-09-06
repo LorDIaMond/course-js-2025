bbd76370177569f3cbb3592645e9e49f3901882d
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 21:40:08 +0300

    01.c.3 done

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 3. IsTicketHappy/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 3. IsTicketHappy/index.js
@@ -1,3 +1,10 @@
-const isTicketHappy = (numStr) => {};
+const isTicketHappy = (numStr) => {
+    let result = 0;
+    for (let i = 0; i < numStr.length / 2; i++) {
+        result += parseInt(numStr[i], 10)
+        result -= parseInt(numStr[numStr.length - 1 - i], 10);
+    }
+    return result === 0;
+};
 
 export default isTicketHappy;
