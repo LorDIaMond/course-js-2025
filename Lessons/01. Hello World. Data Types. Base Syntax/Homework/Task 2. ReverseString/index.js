01a682a679fda3ab066b58b952bbd556c969b9b1
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 20:46:00 +0300

    01.h.2 done

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,7 +1,28 @@
 const reverseString = (str) => {
-    // Начало
-
-    // Конец
+    //Версия поворота строки через цикл
+    //
+    // let result = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     result += str[i];
+    // }
+    // return result;
+// };
+    let strLen = str.length;
+    if (strLen <= 1) {
+        return str;
+    }
+    let rightIndex = strLen - 1;
+    let res = '';
+    if (rightIndex === 1) {
+        res = str[1] + str[0];
+        return res;
+    }
+    if (rightIndex === 2) {
+        res = str[2] + str[1] + str[0];
+        return res;
+    }
+    return reverseString(str.slice(Math.floor(rightIndex / 2), rightIndex + 1)) +
+            reverseString(str.slice(0, Math.floor(rightIndex / 2)));
 };
 
 export default reverseString;
