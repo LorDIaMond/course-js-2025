ef7357ba949999d91c0d5c7eb40b6b8178e31cf5
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-06 21:32:10 +0300

    01.c.2 done

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 2. ChangeCase/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 2. ChangeCase/index.js
@@ -1,3 +1,14 @@
-const changeCase = (str) => {};
+const changeCase = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i].toLowerCase()) {
+            result += str[i].toUpperCase();
+        }
+        else {
+            result += str[i].toLowerCase();
+        }
+    }
+    return result;
+};
 
 export default changeCase;
