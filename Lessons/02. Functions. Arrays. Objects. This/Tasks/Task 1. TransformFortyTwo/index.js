aca57533131a2157d0483ea7956ea95e584b3f80
Aynur.Fattahov <Aynur.Fattahov@tatar>
2025-09-11 11:12:01 +0300

    Занятие 2 | Лекция и практики

---
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 1. TransformFortyTwo/index.js
@@ -0,0 +1,7 @@
+const CHECK_NUM = 42;
+
+const transformFortyTwo = (arr) => {
+
+};
+
+export default transformFortyTwo;
---
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 2. GetHammingWeight/index.js
@@ -0,0 +1,7 @@
+const convertToBinary = num => (num >>> 0).toString(2);
+
+const getHammingWeight = (num) => {
+
+};
+
+export default getHammingWeight;
