c7a02d23a94490c62ea2b2274093cc23f99f91fb
LorDIaMond <lord00alm@gmail.com>
2025-10-02 18:12:47 +0300

    Squashed commit of the following:
    commit dc6e8ef98431829eeb88dc29a0effa3fa00712fc
    Author: Roman.Iskakov <Roman.Iskakov@shire.local>
    Date:   Thu Oct 2 14:46:46 2025 +0300
    
        Занятие 5
    
    commit 080be2a6ca54bb055414db7c6c16d34902af23b9
    Author: Roman.Iskakov <Roman.Iskakov@shire.local>
    Date:   Thu Oct 2 07:38:21 2025 +0300
    
        Новый ignore элемет
    
    commit b9133184a90968ca4c7496c71cf7b5bac8c17d1c
    Author: Roman.Iskakov <Roman.Iskakov@shire.local>
    Date:   Thu Sep 25 14:52:22 2025 +0300
    
        Лекция 4

---
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -0,0 +1,27 @@
+const TYPES = {
+    DIGIT: 'digit',
+    PLUS: 'plus',
+    RESULT: 'result',
+    RESET: 'reset',
+};
+
+const ADDITIONAL_BUTTONS = [
+    {
+        text: '+',
+        type: TYPES.PLUS,
+    },
+    {
+        text: '=',
+        type: TYPES.RESULT,
+    },
+    {
+        text: 'Сбросить',
+        type: TYPES.RESET,
+    },
+];
+
+const setCalculator = () => {
+
+};
+
+export default setCalculator;
---
+++ Lessons/05. DOM. Events/Tasks/Task 3. TrafficLights/index.js
@@ -0,0 +1,28 @@
+const LIGHTS = {
+    red: {
+        type: 'red',
+        text: 'Красный',
+    },
+    yellow: {
+        type: 'yellow',
+        text: 'Желтый',
+    },
+    green: {
+        type: 'green',
+        text: 'Зеленый',
+    },
+};
+
+const resetLights = () => {
+
+};
+
+const setLight = (type) => {
+
+};
+
+const setTrafficLight = () => {
+
+};
+
+export default setTrafficLight;
