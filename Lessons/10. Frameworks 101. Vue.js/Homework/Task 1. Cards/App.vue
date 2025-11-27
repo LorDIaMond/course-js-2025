8137feceeee8b5fedf5d37e2ef7c523cf35180ab
LorDIaMond <lord00alm@gmail.com>
2025-11-27 11:48:34 +0300

    commit

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
@@ -1,33 +1,21 @@
 <template>
   <div class="card-container">
-    <div class="card" v-for="card in products" :key="card.id">
-
-      <img :src="card.src" class="card-img-top" alt="img" />
-
+    <div class="card" v-for="card in cards" :key="card.id">
+      <img alt="img" :src="card.src" class="card-img-top"/>
       <div class="card-body">
-        <h5 class="card-title">
-          {{ card.head }}
-        </h5>
-
-        <h4 class="card-title">
-          {{ card.price }}
-        </h4>
-
-        <p class="card-text">
-          {{ card.description }}
-        </p>
+        <h5 class="card-title">{{ card.head }}</h5>
+        <h4 class="card-title">{{ card.price }}</h4>
+        <p class="card-text">{{ card.description }}</p>
 
         <div class="card__buttons">
-          <button class="btn btn-primary button-plus" @click="inc(card)">+</button>
-          <button class="btn btn-primary button-minus" @click="dec(card)">-</button>
-
+          <button class="btn btn-primary button-plus" @click="inc(card)"> + </button>
+          <button class="btn btn-primary button-minus" @click="dec(card)"> - </button>
           <div>{{ card.number }} шт.</div>
         </div>
 
         <div class="card__alert alert alert-danger" v-if="card.number > card.max">
           Данного товара доступно только {{ card.max }} шт.
         </div>
-
       </div>
     </div>
   </div>
@@ -35,19 +23,18 @@
 
 <script>
 export default {
-  name: 'Cards',
+  name: 'App',
+
+  data() {
+    return {
+    };
+  },
 
   props: {
     cards: {
       type: Array,
-      required: true
-    }
-  },
-
-  computed: {
-    products() {
-      return this.cards;
-    }
+      default: () => [],
+    },
   },
 
   methods: {
